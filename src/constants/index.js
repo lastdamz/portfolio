912b2768aa74004412b5d4f228528a876c2b6e8c
lastdamz <lastmadziwa61@gmail.com>
2025-01-21 11:43:26 +0200

    last commit i guess

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/clausemaxplatform.png";
+import project3 from "../assets/projects/tonderailast.png";
+
 
 export const HERO_CONTENT = `I am a passionate Front-End Developer with 2 years of experience, specializing in creating engaging and intuitive user interfaces. My expertise lies in front-end technologies such as React, where I focus on building responsive and user-friendly web applications. In addition to my development skills, I have a strong interest in UX/UI design, allowing me to craft seamless user experiences that prioritize usability and aesthetics. My goal is to leverage my skills to deliver innovative solutions that not only meet business objectives but also enhance user satisfaction.`;
 
@@ -10,64 +9,32 @@ export const ABOUT_TEXT = `I am a dedicated Front-End Developer with 2 years of 
 My commitment to continuous learning keeps me adaptable to new technologies and design trends. Outside of coding, I enjoy staying active, exploring innovative tools, and contributing to open-source projects to further refine my skills.`;
 
 export const EXPERIENCES = [
+ 
   {
-    year: "2024 - Present",
-    role: "Freelance Graphic Designer",
-    company: "Clausemax Platform",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Photoshop", "React.js", "Adobe Illustrator", "Adobe XD"],
-  },
-  {
-    year: "2022 - 2023",
+    year: "2023 - Present",
     role: "Frontend Developer",
     company: "Curlben Solution",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "React.js", "React Native"],
+    description: `Designed and developed user interfaces for web applications using React. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "React.js",],
   },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Clausemax Platform Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional website for a client, who is a business consultant",
+    technologies: ["HTML", "CSS", "React", "Vite"],
   },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
+ 
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "React", "Javascript"],
   },
 ];
 
