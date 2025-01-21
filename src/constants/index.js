44691cadcc9db4d4ed339656e9719f0314ef8be1
lastdamz <lastmadziwa61@gmail.com>
2025-01-21 12:10:32 +0200

    last commit i guess

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,9 +4,7 @@ import project3 from "../assets/projects/tonderailast.png";
 
 export const HERO_CONTENT = `I am a passionate Front-End Developer with 2 years of experience, specializing in creating engaging and intuitive user interfaces. My expertise lies in front-end technologies such as React, where I focus on building responsive and user-friendly web applications. In addition to my development skills, I have a strong interest in UX/UI design, allowing me to craft seamless user experiences that prioritize usability and aesthetics. My goal is to leverage my skills to deliver innovative solutions that not only meet business objectives but also enhance user satisfaction.`;
 
-export const ABOUT_TEXT = `I am a dedicated Front-End Developer with 2 years of experience in crafting efficient and user-friendly web applications. My expertise lies in technologies such as React, where I focus on creating seamless user interfaces that enhance user experiences. My journey in web development began with a passion for design and usability, driving me to delve into UX/UI principles alongside my technical skills.
-
-My commitment to continuous learning keeps me adaptable to new technologies and design trends. Outside of coding, I enjoy staying active, exploring innovative tools, and contributing to open-source projects to further refine my skills.`;
+export const ABOUT_TEXT = `I am a Front-End Developer with 2 years of experience in creating efficient, user-friendly web applications, specializing in React. My journey began with a passion for design and usability, leading me to explore UX/UI principles alongside my technical skills. I prioritize continuous learning to stay adaptable to new technologies and trends. Outside of coding, I enjoy staying active, exploring innovative tools, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
  
