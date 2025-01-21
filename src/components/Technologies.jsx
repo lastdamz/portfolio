9fbad9cbcef536c59d58687e7d1052d59d6692cf
lastdamz <lastmadziwa61@gmail.com>
2025-01-21 04:15:12 +0200

    first commit

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,31 +5,77 @@ import { SiAdobexd } from "react-icons/si";
 import { SiFramer } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { FaFigma } from "react-icons/fa";
+import {motion} from "framer-motion"
+
+const iconVariants = (duration) => ({
+  initial: {y: -10},
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    }, 
+  },
+});
 
 const Technologies = () => {
   return (
     <div>
-      <h2 className='my-20 text-center text-4xl'>Technologies & <span className='text-neutral-500'>Tools</span></h2>
-      <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+      <motion.h2 
+      whileInView={{opacity:1 , y: 0}}
+      initial={{opacity: 0, y: -100}}
+      transition={{duration: 1.5}}
+      className='my-20 text-center text-4xl'>Technologies & <span className='text-neutral-500'>Tools</span></motion.h2>
+      <motion.div
+      whileInView={{opacity:1 , x: 0}}
+      initial={{opacity: 0, x: -100}}
+      transition={{duration: 1.5}}
+      className='flex flex-wrap items-center justify-center gap-4'>
+        <motion.div 
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <SiAdobephotoshop className='text-6xl text-cyan-600'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(3)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <SiAdobeillustrator className='text-6xl text-orange-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div
+        variants={iconVariants(5)}
+        initial="initial"
+        animate="animate" 
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <SiAdobexd className='text-6xl text-purple-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div
+        variants={iconVariants(2)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <SiFramer  className='text-6xl text-purple-100'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div
+        variants={iconVariants(6)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <FaReact className='text-6xl text-cyan-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div
+        variants={iconVariants(4)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
             <FaFigma className='text-6xl text-purple-100'/>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
